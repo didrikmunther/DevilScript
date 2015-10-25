1a380d5ac9ad3be4473cc71f46cadc61c15408c4
Azinum <lucaskarlsson@Gunnar-the-Great.local>
2015-10-25 17:48:55 +0100

    Added script execution

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <iostream>
-
+#include <fstream>
 #include "Lexer.h"
 #include "Parser.h"
 #include "Stack.h"
@@ -21,15 +21,39 @@ int main(int argc, const char * argv[]) {
     Parser parser;
     Stack stack;
     
+    std::ifstream file;
+    std::string line;
+    
     while(true) {
         std::cout << ">> ";
         std::string input = "";
         std::getline(std::cin, input);
+        
         if(input == "quit") break;
         
-        std::string output = parser.parse(lexer.lex(input), &stack, &lexer);
-        std::cout << output << (output == "" ? "" : "\n");
+        if (input == "import") {
+            std::cout << "[open]: ";
+            std::getline(std::cin, input);
+            
+            file.open(input);
+            
+            if (!file) {
+                std::cout << "[Error]: could not execute script" << "\n";
+                continue;
+            }
+            
+            if (file.is_open()) {
+                std::cout << "[Success]: opened script" << "\n\n";
+                while (std::getline(file,line)) {
+                    std::string output = parser.parse(lexer.lex(line), &stack, &lexer);
+                    std::cout << output << (output == "" ? "" : "\n");
+                }
+            }
+        } else {
+            std::string output = parser.parse(lexer.lex(input), &stack, &lexer);
+            std::cout << output << (output == "" ? "" : "\n");
+        }
     }
-    
+    file.close();
     return 0;
 }
